8e673e9dff2b9d6f5abeadbb82b6f0557f6a4ea1
thexovc <osazeepeter79@gmail.com>
2023-06-24 08:21:55 +0100

    added custom token to the duniswap contract and integrating the frontend

--- smart contract/scripts/deployV1.js
+++ smart contract/scripts/deployV1.js
@@ -7,17 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Duni = await hre.ethers.getContractFactory("DUNISWAP_EXCHANGE");
+  const duni = await Duni.deploy();
 
-  const tokenAddr = "0x7f174aA93129031A0a2404304329357E2ed95E97";
+  await duni.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("ExchangeV1");
-  const lock = await Lock.deploy(tokenAddr);
-
-  await lock.deployed();
-
-  console.log(`ExchangeV1 token addr ${tokenAddr} deployed to ${lock.address}`);
+  console.log(`DUNISWAP_EXCHANGE deployed to ${duni.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
